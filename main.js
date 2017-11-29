6766779b7e1b964408d3fb5021cd0ec9fb38c836
ChrisMFernandes <Chrismfernandes12@gmail.com>
2017-11-29 12:13:48 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log('linked');
+
+// Build Deck
+
+// Shuffle Function
+
+// Create Players 
+
+// Bets Function
+
+// Deal Function
+
+// Render Cards
+
+// Hit Function
+
+// Stay/Call Function
+
+// Win Function
