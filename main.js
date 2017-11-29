2ca9f86b93dd09e62dc0915a99af90b1730582a5
ChrisMFernandes <Chrismfernandes12@gmail.com>
2017-11-29 12:35:22 -0500

    added more psueudocode

--- main.js
+++ main.js
@@ -2,18 +2,36 @@ console.log('linked');
 
 // Build Deck
 
+	// suits, values, deck
+
 // Shuffle Function
+
+	// find algorithm
 
 // Create Players 
 
+	// player and dealer
+
 // Bets Function
+
+	// add monetary value plus functionality that adds to pot if hand won
 
 // Deal Function
 
+	// deal two players two cards each
+
 // Render Cards
+
+	// image for front and back of 52 cards
 
 // Hit Function
 
+	// adds one card to player hand plus adds value of card dealt
+
 // Stay/Call Function
 
+	// should trigger game function
+
 // Win Function
+
+	// rounds won or money earned shoulf trigger this function?
